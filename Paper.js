9c18bac45b064a106602abe046704674e148c5e0
santhosh776 <77959315+santhosh776@users.noreply.github.com>
2021-02-25 15:16:09 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,26 @@
+class Paper {
+    constructor(x, y) {
+      var options = {
+          isStatic:false,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.body = Bodies.circle(x, y,3, options);
+      this.radius = 3;
+      
+      
+      World.add(world, this.body);
+      this.image=loadImage("Paper.png")
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image,0, 0, this.width, this.height);
+      pop();
+    }
+  };
