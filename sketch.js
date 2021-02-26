2b59b74f992e4ad175ef3d446b7f82e6ebc29bd8
santhosh776 <77959315+santhosh776@users.noreply.github.com>
2021-02-26 16:09:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function draw() {
 function keyPressed(){
   if(keyCode === UP_ARROW){
 
-  Matter.Body.applyForce(paper.body,paper.body.position,{x:10,y:-10});
+  Matter.Body.applyForce(paper.body,paper.body.position,{x:2,y:-1.7});
   
   }
 }
